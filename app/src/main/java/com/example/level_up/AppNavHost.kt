4d7cf74e72a64acb066eac245a35157a6d769473
Fernando-du <fernan.martineza@duocuc.cl>
2025-10-20 02:55:21 -0300

    pantalla principal v.0.6 reseña

--- app/src/main/java/com/example/level_up/AppNavHost.kt
+++ app/src/main/java/com/example/level_up/AppNavHost.kt
@@ -1,6 +1,5 @@
 package com.example.level_up
 
-
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -13,8 +12,8 @@ import com.example.level_up.viewmodel.ProductViewModel
 @Composable
 fun AppNavHost(
     navController: NavHostController,
-    productViewModel: ProductViewModel,
-    cartViewModel: CartViewModel
+    cartViewModel: CartViewModel,
+    productViewModel: ProductViewModel
 ) {
     NavHost(navController = navController, startDestination = "home") {
         composable("home") {
