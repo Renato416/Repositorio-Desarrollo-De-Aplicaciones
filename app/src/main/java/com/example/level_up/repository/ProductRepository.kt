cbe00c4a7a90e783aa8668e71a29293cba75753f
Fernando-du <fernan.martineza@duocuc.cl>
2025-10-20 01:54:22 -0300

    pantalla principal v.0.3 Nav

--- app/src/main/java/com/example/level_up/repository/ProductRepository.kt
+++ app/src/main/java/com/example/level_up/repository/ProductRepository.kt
@@ -1,13 +1,15 @@
 package com.example.level_up.repository
 
-
-
 import com.example.level_up.R
 import com.example.level_up.model.Product
 
 object ProductRepository {
     fun getProducts() = listOf(
-        Product(1, "Audífonos GAMER", 64990.0, R.drawable.audifonos),
-        Product(2, "Silla GAMER", 72990.0, R.drawable.silla)
+        Product(1, "Audífonos GAMER", 20990.0, R.drawable.audifonos),
+        Product(2, "Silla GAMER", 49990.0, R.drawable.silla),
+        Product(3, "Mouse GAMER", 14990.0, R.drawable.mouse),
+        Product(4, "Teclado GAMER", 30990.0, R.drawable.teclado),
+        Product(5, "Pantalla GAMER", 90990.0, R.drawable.pantalla),
+        Product(6, "Mando GAMER", 25990.0, R.drawable.mando)
     )
 }
